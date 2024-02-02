861cec51f33b237982f13cd78b8ae98ee6842b72
pabather <p.a.bather1@gmail.com>
2024-02-02 22:39:43 +0100

    added teams contract

--- app/src/main/java/com/philippabather/ligaapp/contract/TeamsContract.java
+++ app/src/main/java/com/philippabather/ligaapp/contract/TeamsContract.java
@@ -1,2 +1,26 @@
-package com.philippabather.ligaapp.contract;public class TeamsContract {
+package com.philippabather.ligaapp.contract;
+
+import com.philippabather.ligaapp.domain.Team;
+
+import java.util.List;
+public interface TeamsContract {
+
+    interface Model {
+
+        interface OnLoadTeamsListener {
+            void onLoadTeamsSuccess(List<Team> teamsList);
+            void onLoadTeamsError(String msg);
+        }
+        void loadTeams(OnLoadTeamsListener onLoadTeamsListener);
+    }
+
+    interface View {
+        void listTeams(List<Team> teamsList);
+        void showMessage(String msg);
+    }
+
+    interface Presenter {
+        void loadTeams();
+    }
+
 }
