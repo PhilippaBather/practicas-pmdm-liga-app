e4e817647e8bc37cd75750c3bd75dd01348da48b
pabather <p.a.bather1@gmail.com>
2024-02-02 22:40:16 +0100

    added getters and setters

--- app/src/main/java/com/philippabather/ligaapp/domain/Team.java
+++ app/src/main/java/com/philippabather/ligaapp/domain/Team.java
@@ -8,4 +8,44 @@ public class Team {
     private String foundationDate;
     private Boolean areChampions;
     private int leaguePoints;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFoundationDate() {
+        return foundationDate;
+    }
+
+    public void setFoundationDate(String foundationDate) {
+        this.foundationDate = foundationDate;
+    }
+
+    public Boolean getAreChampions() {
+        return areChampions;
+    }
+
+    public void setAreChampions(Boolean areChampions) {
+        this.areChampions = areChampions;
+    }
+
+    public int getLeaguePoints() {
+        return leaguePoints;
+    }
+
+    public void setLeaguePoints(int leaguePoints) {
+        this.leaguePoints = leaguePoints;
+    }
 }
