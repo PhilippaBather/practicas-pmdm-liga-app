c1e58ee633f29fd4cb8d77977567bf436738cdfa
pabather <p.a.bather1@gmail.com>
2024-02-02 21:29:50 +0100

    added presenter

--- app/src/main/java/com/philippabather/ligaapp/presenter/StadiumPresenter.java
+++ app/src/main/java/com/philippabather/ligaapp/presenter/StadiumPresenter.java
@@ -1,2 +1,34 @@
-package com.philippabather.ligaapp.presenter;public class StadiumPresenter {
+package com.philippabather.ligaapp.presenter;
+
+import com.philippabather.ligaapp.contract.StadiumsContract;
+import com.philippabather.ligaapp.domain.Stadium;
+import com.philippabather.ligaapp.model.StadiumModel;
+import com.philippabather.ligaapp.views.StadiumsView;
+
+import java.util.List;
+
+public class StadiumPresenter implements StadiumsContract.Presenter, StadiumsContract.Model.OnLoadStadiumsListener {
+
+    private StadiumModel stadiumModel;
+    private StadiumsView stadiumsView;
+
+    public StadiumPresenter(StadiumsView stadiumsView) {
+        this.stadiumsView = stadiumsView;
+        this.stadiumModel = new StadiumModel(this.stadiumsView);
+    }
+
+    @Override
+    public void loadStadiums() {
+        stadiumModel.loadStadiums(this);
+    }
+
+    @Override
+    public void onLoadStadiumsSuccess(List<Stadium> stadiumList) {
+        stadiumsView.listStadiums(stadiumList);
+    }
+
+    @Override
+    public void onLoadStadiumsError(String msg) {
+        stadiumsView.showMessage(msg);
+    }
 }
