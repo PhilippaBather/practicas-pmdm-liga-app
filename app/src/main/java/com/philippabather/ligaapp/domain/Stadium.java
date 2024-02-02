95654a895c3342511516134d634daa956a7f2abb
pabather <p.a.bather1@gmail.com>
2024-02-02 18:08:53 +0100

    added classes

---
+++ app/src/main/java/com/philippabather/ligaapp/domain/Stadium.java
@@ -0,0 +1,13 @@
+package com.philippabather.ligaapp.domain;
+
+import java.time.LocalDate;
+
+public class Stadium {
+
+    private long id;
+    private String name;
+    private LocalDate constructionDate;
+    private Boolean adaptedAccess;
+    private float latitude;
+    private float longitude;
+}
---
+++ app/src/main/java/com/philippabather/ligaapp/domain/Team.java
@@ -0,0 +1,11 @@
+package com.philippabather.ligaapp.domain;
+
+import java.time.LocalDate;
+
+public class Team {
+    private long id;
+    private String name;
+    private LocalDate foundationDate;
+    private Boolean areChampions;
+    private int leaguePoints;
+}
