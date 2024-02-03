d1d05b56a94f59c64a50ec1bb0e4891f4b2493db
pabather <p.a.bather1@gmail.com>
2024-02-03 14:02:35 +0100

    added cntrct

--- app/src/main/java/com/philippabather/ligaapp/contract/AddStadiumContract.java
+++ app/src/main/java/com/philippabather/ligaapp/contract/AddStadiumContract.java
@@ -1,2 +1,25 @@
-package com.philippabather.ligaapp.contract;public interface AddStadiumContract {
+package com.philippabather.ligaapp.contract;
+
+import com.philippabather.ligaapp.domain.NewStadiumDTO;
+import com.philippabather.ligaapp.domain.Stadium;
+
+public interface AddStadiumContract {
+
+    interface Model {
+
+        interface OnAddStadiumListener {
+            void onAddStadiumSuccess(Stadium stadium);
+            void onAddStadiumError(String msg);
+        }
+
+        void addStadium(OnAddStadiumListener onAddStadiumListener, NewStadiumDTO stadium);
+    }
+
+    interface View {
+        void showMessage(String msg);
+    }
+
+    interface Presenter {
+        void addStadiumByTeamId(NewStadiumDTO stadium);
+    }
 }
