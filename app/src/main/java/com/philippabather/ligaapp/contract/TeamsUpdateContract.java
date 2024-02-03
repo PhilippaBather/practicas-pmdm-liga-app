7ec86a605c6cc3a342b90cbdba6091a434170ce7
pabather <p.a.bather1@gmail.com>
2024-02-03 12:02:53 +0100

    added update contract

--- app/src/main/java/com/philippabather/ligaapp/contract/TeamsUpdateContract.java
+++ app/src/main/java/com/philippabather/ligaapp/contract/TeamsUpdateContract.java
@@ -1,2 +1,32 @@
-package com.philippabather.ligaapp.contract;public interface TeamsUpdateContract {
+package com.philippabather.ligaapp.contract;
+
+import com.philippabather.ligaapp.domain.Team;
+
+public interface TeamsUpdateContract {
+
+    interface Model {
+
+        interface OnUpdateTeamListener {
+            void onUpdateTeamSuccess(Team team);
+            void onUpdateTeamError(String msg);
+        }
+
+        interface OnDeleteTeamListener {
+            void onDeleteTeamSuccess();
+            void onDeleteTeamError(String msg);
+        }
+
+        void updateTeamById(OnUpdateTeamListener updateTeamListener, long id, Team team);
+        void deleteTeamById(OnDeleteTeamListener deleteTeamListener, long id);
+
+    }
+
+    interface View {
+        void showMessage(String msg);
+    }
+
+    interface Presenter {
+        void updateTeamById(long id, Team team);
+        void deleteTeamById(long id);
+    }
 }
