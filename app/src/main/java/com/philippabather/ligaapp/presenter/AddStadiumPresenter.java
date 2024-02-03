64f779246e3f4ec91d7c18480a40f46bca5dd205
pabather <p.a.bather1@gmail.com>
2024-02-03 14:03:10 +0100

    added presenter

--- app/src/main/java/com/philippabather/ligaapp/presenter/AddStadiumPresenter.java
+++ app/src/main/java/com/philippabather/ligaapp/presenter/AddStadiumPresenter.java
@@ -1,2 +1,34 @@
-package com.philippabather.ligaapp.presenter;public class AddStadiumPresenter {
+package com.philippabather.ligaapp.presenter;
+
+import com.philippabather.ligaapp.contract.AddStadiumContract;
+import com.philippabather.ligaapp.domain.NewStadiumDTO;
+import com.philippabather.ligaapp.domain.Stadium;
+import com.philippabather.ligaapp.model.AddStadiumModel;
+import com.philippabather.ligaapp.views.AddStadiumView;
+
+public class AddStadiumPresenter implements AddStadiumContract.Presenter, AddStadiumContract.Model.OnAddStadiumListener {
+
+    private AddStadiumView view;
+
+    private AddStadiumModel model;
+
+    public AddStadiumPresenter(AddStadiumView view) {
+        this.view = view;
+        this.model = new AddStadiumModel();
+    }
+
+    @Override
+    public void onAddStadiumSuccess(Stadium stadium) {
+        view.showMessage("Added");
+    }
+
+    @Override
+    public void onAddStadiumError(String msg) {
+        view.showMessage(msg);
+    }
+
+    @Override
+    public void addStadiumByTeamId(NewStadiumDTO stadium) {
+        model.addStadium(this, stadium);
+    }
 }
