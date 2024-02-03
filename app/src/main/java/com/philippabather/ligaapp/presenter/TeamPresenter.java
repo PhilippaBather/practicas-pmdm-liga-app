cab98a971a9b2d258d772d7d635cd8587cd8b0bd
pabather <p.a.bather1@gmail.com>
2024-02-03 09:57:30 +0100

    updated

--- app/src/main/java/com/philippabather/ligaapp/presenter/TeamPresenter.java
+++ app/src/main/java/com/philippabather/ligaapp/presenter/TeamPresenter.java
@@ -2,25 +2,25 @@ package com.philippabather.ligaapp.presenter;
 
 import com.philippabather.ligaapp.contract.TeamsContract;
 import com.philippabather.ligaapp.domain.Team;
-import com.philippabather.ligaapp.model.TeamModel;
+import com.philippabather.ligaapp.model.TeamsModel;
 import com.philippabather.ligaapp.views.TeamsView;
 
 import java.util.List;
 
 public class TeamPresenter implements TeamsContract.Presenter, TeamsContract.Model.OnLoadTeamsListener {
 
-    private TeamModel teamModel;
+    private TeamsModel teamsModel;
     private TeamsView teamsView;
 
 
     public TeamPresenter(TeamsView teamsView) {
         this.teamsView = teamsView;
-        this.teamModel = new TeamModel();
+        this.teamsModel = new TeamsModel();
     }
 
     @Override
     public void loadTeams() {
-        teamModel.loadTeams(this);
+        teamsModel.loadTeams(this);
     }
     @Override
     public void onLoadTeamsSuccess(List<Team> teamsList) {
--- app/src/main/java/com/philippabather/ligaapp/presenter/TeamsDetailPresenter.java
+++ app/src/main/java/com/philippabather/ligaapp/presenter/TeamsDetailPresenter.java
@@ -1,2 +1,32 @@
-package com.philippabather.ligaapp.presenter;public class TeamsDetailPresenter {
+package com.philippabather.ligaapp.presenter;
+
+import com.philippabather.ligaapp.contract.TeamDetailContract;
+import com.philippabather.ligaapp.domain.Team;
+import com.philippabather.ligaapp.model.TeamDetailModel;
+import com.philippabather.ligaapp.views.TeamDetailView;
+
+public class TeamsDetailPresenter implements TeamDetailContract.Presenter, TeamDetailContract.Model.OnLoadTeamListener {
+
+    private TeamDetailModel teamDetailModel;
+    private TeamDetailView teamDetailView;
+
+    public TeamsDetailPresenter(TeamDetailView teamDetailView) {
+        this.teamDetailView = teamDetailView;
+        this.teamDetailModel = new TeamDetailModel();
+    }
+
+    @Override
+    public void onLoadTeamSuccess(Team team) {
+        teamDetailView.listTeam(team);
+    }
+
+    @Override
+    public void onLoadTeamError(String msg) {
+        teamDetailView.showMessage(msg);
+    }
+
+    @Override
+    public void loadTeam(long id) {
+        teamDetailModel.loadTeam(this, id);
+    }
 }
